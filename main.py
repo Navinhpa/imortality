799a595cbb6e714036204d6fe237bd7fecdfc62c
Navinhpa <navin131214xx@gmail.com>
2026-03-13 21:17:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Import pandas as pd
 from sklearn.model selection import train test split
 from sklearn.linear model import LinearRegression
 Load data from CSV (Ensure the file has "HouraStudied" and "ExamScore" columns)
-df pd.read_csv("data.csv")
+df pd.read_csv("Data.csv")
 Split data into Features (X) and Target (y)
 Xdf [['HoursStudied']]
 y df ['ExamScore']
